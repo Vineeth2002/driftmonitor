f2e7bc10d91ecba0d5ea757b9ab1bd476290069f
Vineeth2002 <83575425+Vineeth2002@users.noreply.github.com>
2025-12-11 22:22:23 +0530

    Create __init__.py

---
+++ collectors/template/__init__.py
@@ -0,0 +1,8 @@
+"""
+DriftMonitor - Collector Template Package
+
+A reusable template for building new collectors quickly. Use this as the
+basis for any custom data source collector (APIs, scrapers, uploads).
+"""
+
+__all__ = ["TemplateCollector"]
